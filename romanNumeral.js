d306c929024ca363ec1739fee45f910f1682878e
Kat Leight <leightkt@gmail.com>
2021-04-16 13:41:21 -0600

    write dynamic regex expression for romanNumeral

--- romanNumeral.js
+++ romanNumeral.js
@@ -1,6 +1,25 @@
 function romanNumeral(roman) {
-    return 1
+    const replaceOptions = {
+        "I": '1,',
+        "IV": '4,',
+        "V": '5,',
+        "IX": '9,',
+        "X": '10,',
+        "XL": '40,',
+        "L": '50,',
+        "XC": '90,',
+        "C": '100,',
+        "CD": '400,',
+        "D": '500,',
+        "CM": '900,',
+        "M": '1000,'
+    }
+    const reg = new RegExp(Object.keys(replaceOptions).join("|"),"gi");
+    return roman.replace(reg, function(matched) {
+        return replaceOptions[matched]
+    })
 }
+
 
 module.exports =  {
     romanNumeral
