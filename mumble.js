9ca6a1f82ba16d53a6ff52be007d95ec560e4e62
Kat Leight <leightkt@gmail.com>
2021-04-20 10:08:50 -0600

    add additional for loop to mumble solution, passing initial tests

--- mumble.js
+++ mumble.js
@@ -1,12 +1,18 @@
 function mumble (string) {
     const letterArray =  string.split('').map(letter => letter.toUpperCase())
     for (i = 0; i < letterArray.length; i++) {
-        if (i > 0) {
-            letterArray[i] = letterArray[i].concat( (letterArray[i].toLowerCase()) )
+        let times = i
+        let string = ''
+        for (y = 0; y < times; y++) {
+            string = string.concat( returnLetter(letterArray[i]) )
         }
-        
+        letterArray[i] = letterArray[i].concat(string)
     }
-    return letterArray
+    return letterArray.join('-')
+}
+
+function returnLetter (letter) {
+    return letter.toLowerCase()
 }
 
 module.exports = {
