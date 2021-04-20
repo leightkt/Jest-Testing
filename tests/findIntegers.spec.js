54200fd5b1c6741dc4ea552b49da43d6222c3e11
Kat Leight <leightkt@gmail.com>
2021-04-20 10:29:17 -0600

    add two additional tests for findIntegers

--- tests/findIntegers.spec.js
+++ tests/findIntegers.spec.js
@@ -11,3 +11,15 @@ describe("findIntegers", () => {
         expect(findIntegers([1, 2, 3])).toBe(2)
     })
 })
+
+describe("findIntegers", () => {
+    it("findIntegers([2,6,8,10,3]) => 3", () => {
+        expect(findIntegers([2,6,8,10,3])).toBe(3)
+    })
+})
+
+describe("findIntegers", () => {
+    it("findIntegers([0,0,3,0,0]) => 3", () => {
+        expect(findIntegers([0,0,3,0,0])).toBe(3)
+    })
+})
