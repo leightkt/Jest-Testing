e974c4ebc71c767880e10c9beb9f5e189c69218e
Kat Leight <leightkt@gmail.com>
2021-04-20 10:54:15 -0600

    working solution for findIntegers

--- findIntegers.js
+++ findIntegers.js
@@ -1,13 +1,13 @@
 function findIntegers (array) {
     let even = false
     if ( array[0] % 2 === 0 ) {
-        if ( array[1] % 2 === 0 || array[2] === [0] ) {
+        if ( array[1] % 2 === 0 || array[2] % 2 === 0 ) {
             even = true
         }
     }
 
     if (even) {
-        return array.find(num => num % 2 !== 0)
+        return array.find(num => num % 2 !== 0 )
     } else {
         return array.find(num => num % 2 === 0)
     }
