3a80c6309292551147b7d036cecdb3e9987877c5
Kat Leight <leightkt@gmail.com>
2021-04-20 10:37:49 -0600

    initial solution- does not accout for 0

--- findIntegers.js
+++ findIntegers.js
@@ -1,5 +1,16 @@
-function findIntegers () {
-    return 1
+function findIntegers (array) {
+    let even = false
+    if ( array[0] % 2 === 0 ) {
+        if ( array[1] % 2 === 0 || array[2] === [0] ) {
+            even = true
+        }
+    }
+
+    if (even) {
+        return array.find(num => num % 2 !== 0)
+    } else {
+        return array.find(num => num % 2 === 0)
+    }
 }
 
 module.exports = {
