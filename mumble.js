817b439e07e040314daa6ac4134a5e6649178969
Kat Leight <leightkt@gmail.com>
2021-04-20 10:17:40 -0600

    refactor mumble with .repeat

--- mumble.js
+++ mumble.js
@@ -1,19 +1,11 @@
 function mumble (string) {
     const letterArray =  string.split('').map(letter => letter.toUpperCase())
     for (i = 0; i < letterArray.length; i++) {
-        let times = i
-        let string = ''
-        for (y = 0; y < times; y++) {
-            string = string.concat( returnLetter(letterArray[i]) )
-        }
-        letterArray[i] = letterArray[i].concat(string)
+        letterArray[i] = letterArray[i] + letterArray[i].toLowerCase().repeat(i)
     }
     return letterArray.join('-')
 }
 
-function returnLetter (letter) {
-    return letter.toLowerCase()
-}
 
 module.exports = {
     mumble
