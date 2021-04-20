df85d82845a0cbc1ec944b8e7392bc35bd658baa
Kat Leight <leightkt@gmail.com>
2021-04-20 09:26:04 -0600

    reorder replaceOptions so that two letter IV etc come first

--- romanNumeral.js
+++ romanNumeral.js
@@ -1,17 +1,17 @@
 function romanNumeral(roman) {
     const replaceOptions = {
-        "I": '1,',
         "IV": '4,',
-        "V": '5,',
         "IX": '9,',
-        "X": '10,',
         "XL": '40,',
-        "L": '50,',
         "XC": '90,',
-        "C": '100,',
         "CD": '400,',
-        "D": '500,',
         "CM": '900,',
+        "I": '1,',
+        "V": '5,',
+        "X": '10,',
+        "L": '50,',
+        "C": '100,',
+        "D": '500,',
         "M": '1000,'
     }
     const reg = new RegExp(Object.keys(replaceOptions).join("|"),"g");
