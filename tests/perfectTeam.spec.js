e770b72195490fdf0a6992e00c82d9c6d83237b8
Kat Leight <leightkt@gmail.com>
2021-04-28 15:18:19 -0600

    update first test to include second arguement

--- tests/perfectTeam.spec.js
+++ tests/perfectTeam.spec.js
@@ -1,5 +1,7 @@
 const { perfectTeam } = require('../perfectTeam')
 
 describe('perfectTeam', () => {
-    it('perfectTeam')
+    it('perfectTeam(3, [[0,2], [1,2], [0,1], [0]]) => 3', () => {
+        expect(perfectTeam(3, [[0,2], [1,2], [0,1], [0]])).toBe(3)
+    })
 })
