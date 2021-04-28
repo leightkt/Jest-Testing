a71fd3e64edce495226be0a52e756a9178725b12
Kat Leight <leightkt@gmail.com>
2021-04-28 15:19:55 -0600

    write second test for perfectTeam

--- tests/perfectTeam.spec.js
+++ tests/perfectTeam.spec.js
@@ -5,3 +5,9 @@ describe('perfectTeam', () => {
         expect(perfectTeam(3, [[0,2], [1,2], [0,1], [0]])).toBe(3)
     })
 })
+
+describe('perfectTeam', () => {
+    it('perfectTeam(6,[[0,1],  [1,3],  [0,2],  [0,5],  [1,4]]) => 5', () => {
+        expect(perfectTeam(6,[[0,1],  [1,3],  [0,2],  [0,5],  [1,4]])).toBe(5)
+    })
+})
