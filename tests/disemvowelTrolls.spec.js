4f8f46b8e197461b69c298e879a2606d7cb2856e
Kat Leight <leightkt@gmail.com>
2021-04-29 10:11:08 -0600

    write second and third test for disemvowelTrolls

--- tests/disemvowelTrolls.spec.js
+++ tests/disemvowelTrolls.spec.js
@@ -1,5 +1,19 @@
 const { disemvowelTrolls } = require("../disemvowelTrolls");
 
 describe('disemvowelTrolls', () => {
-    it('dis')
+    it('disemvowelTrolls("This website is for losers LOL!") => "Ths wbst s fr lsrs LL!"', () => {
+        expect(disemvowelTrolls("This website is for losers LOL!")).toEqual("Ths wbst s fr lsrs LL!")
+    })
+})
+
+describe('disemvowelTrolls', () => {
+    it('disemvowelTrolls("No offense but,\nYour writing is among the worst I\'ve ever read") => "N ffns bt,\nYr wrtng s mng th wrst \'v vr rd"', () => {
+        expect(disemvowelTrolls("No offense but,\nYour writing is among the worst I\'ve ever read")).toEqual("N ffns bt,\nYr wrtng s mng th wrst \v vr rd")
+    })
+})
+
+describe('disemvowelTrolls', () => {
+    it('disemvowelTrolls("What are you, a communist?") => "Wht r y,  cmmnst"', () => {
+        expect(disemvowelTrolls("What are you, a communist?")).toEqual("Wht r y,  cmmnst")
+    })
 })
