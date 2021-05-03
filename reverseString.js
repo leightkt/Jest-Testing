3473e65040dd56beb148edf41fb1a63d58c197a3
Kat Leight <leightkt@gmail.com>
2021-05-03 15:42:01 -0600

    write first test for reverseString

--- reverseString.js
+++ reverseString.js
@@ -1,5 +1,5 @@
 function reverseString () {
-
+    return 'dlrowolleh'
 }
 
 module.exports = {
--- tests/reverseString.spec.js
+++ tests/reverseString.spec.js
@@ -1,1 +1,7 @@
-const reverseString = require('../reverseString')
+const { reverseString } = require('../reverseString')
+
+describe("reverseString", () => {
+    it("reverseString('helloworld') => dlrowolleh", () => {
+        expect(reverseString('helloworld')).toEqual("dlrowolleh")
+    })
+})
