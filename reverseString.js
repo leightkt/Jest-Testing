58a06588f431f1b06b44395ac786b93c0e33ce74
Kat Leight <leightkt@gmail.com>
2021-05-03 15:53:08 -0600

    initial solution for reverseString

--- reverseString.js
+++ reverseString.js
@@ -1,5 +1,13 @@
-function reverseString () {
-    return 'dlrowolleh'
+function reverseString (string) {
+    string = string.toLowerCase()
+
+    for (i = 0; i < string.length / 2; i++) {
+        let letter = string[i]
+        string[i] = string[string.length - i - 1]
+        string[string.length - i - 1] = letter
+    }
+
+    return string
 }
 
 module.exports = {
